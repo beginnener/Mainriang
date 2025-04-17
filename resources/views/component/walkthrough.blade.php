c13eb4a7385e4bd5aa7d90b306a5cff786cfe583
nenematoda <natashadind@upi.edu>
2025-04-18 05:03:00 +0700

    add banne.pngr & walkthrough

---
+++ resources/views/component/walkthrough.blade.php
@@ -0,0 +1,47 @@
+<div class="w-[1440px] h-60 left-0 absolute">
+    <div class="w-[1478px] h-14 left-[-19px] top-[56px] absolute inline-flex justify-center items-center gap-2.5">
+        <div class="w-28 h-0 outline outline-purple-950"></div>
+        <div class="w-7 h-7 bg-purple-950 rounded-full">
+            <div class="w-3.5 h-6 px-2 text-center justify-center text-white text-xl font-bold font-['Poppins']">1</div>
+        </div>
+        <div class="w-28 h-0 outline outline-purple-950"></div>
+        <div class="w-7 h-7 bg-purple-950 rounded-full">
+            <div class="w-3.5 h-6 px-2 text-center justify-center text-white text-xl font-bold font-['Poppins']">2</div>
+        </div>
+        <div class="w-28 h-0 outline outline-purple-950"></div>
+        <div class="w-7 h-7 bg-purple-950 rounded-full"vv>
+            <div class="w-3.5 h-6 px-2 text-center justify-center text-white text-xl font-bold font-['Poppins']">3</div>
+        </div>
+        <div class="w-28 h-0 outline outline-purple-950"></div>
+        <div class="w-7 h-7 bg-purple-950 rounded-full">
+            <div class="w-3.5 h-6 px-2 text-center justify-center text-white text-xl font-bold font-['Poppins']">4</div>
+        </div>
+        <div class="w-28 h-0 outline outline-purple-950"></div>
+        <div class="w-7 h-7 bg-purple-950 rounded-full">
+            <div class="w-3.5 h-6 px-2 text-center justify-center text-white text-xl font-bold font-['Poppins']">5</div>
+        </div>
+        <div class="w-28 h-0 outline outline-purple-950"></div>
+        <div class="w-7 h-7 bg-purple-950 rounded-full">
+            <div class="w-3.5 h-6 px-2 text-center justify-center text-white text-xl font-bold font-['Poppins']">6</div>
+        </div>
+        <div class="w-28 h-0 outline outline-purple-950"></div>
+        <div class="w-7 h-7 bg-purple-950 rounded-full">
+            <div class="w-3.5 h-6 px-2 text-center justify-center text-white text-xl font-bold font-['Poppins']">7</div>
+        </div>
+        <div class="w-28 h-0 outline outline-purple-950"></div>
+        <div class="w-7 h-7 bg-purple-950 rounded-full">
+            <div class="w-3.5 h-6 px-2 text-center justify-center text-white text-xl font-bold font-['Poppins']">8</div>
+        </div>
+        <div class="w-28 h-0 outline"></div>
+    </div>
+    <div class="left-[81px] top-[112px] absolute inline-flex justify-center items-start gap-8">
+        <div class="w-32 text-center justify-center text-black text-base font-bold font-['Poppins']">Input data diri anak</div>
+        <div class="w-32 text-center justify-center text-ungu-Loading text-base font-bold font-['Poppins']">Input data diri Orangtua/Wali</div>
+        <div class="w-32 text-center justify-center text-ungu-Loading text-base font-bold font-['Poppins']">Konfirmasi Pembayaran Pendaftaran</div>
+        <div class="w-32 text-center justify-center text-ungu-Loading text-base font-bold font-['Poppins']">Pemilihan Program</div>
+        <div class="w-32 text-center justify-center text-ungu-Loading text-base font-bold font-['Poppins']">Pengisian Form Lanjutan</div>
+        <div class="w-32 text-center justify-center text-ungu-Loading text-base font-bold font-['Poppins']">Asesmen & Wawancara Oraangtua</div>
+        <div class="w-32 text-center justify-center text-ungu-Loading text-base font-bold font-['Poppins']">Konfirmasi Data</div>
+        <div class="w-32 text-center justify-center text-ungu-Loading text-base font-bold font-['Poppins']">Pembayaran Pendidikan</div>
+    </div>
+</div>
