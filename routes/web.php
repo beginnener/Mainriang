a1bd39e65c25c48d45b22059365e74deb7b7ea32
nenematoda <natashadind@upi.edu>
2025-04-17 13:46:45 +0700

    routing placeholder

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('homepage');
 });
+Route::get('/pendaftaran', function () {
+    return view('pendaftaran');
+});
+Route::get('/pendaftaran/data-anak', function () {
+    return view('pendaftaran-dataAnak');
+});
+Route::get('/pendaftaran/data-ortu', function () {
+    return view('pendaftaran-dataOrtu');
+});
+Route::get('/pendaftaran/konfirmasi-pembayaran1', function () {
+    return view('pendaftaran-konfirmasiBayar');
+});
+Route::get('/pendaftaran/form-lanjutan', function () {
+    return view('pendaftaran-formLanjutan');
+});
