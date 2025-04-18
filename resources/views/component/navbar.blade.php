2af5e6c0afda2f48f3147e99847822cdae86bc9a
blayrublay <106981669+aqsayangkamu@users.noreply.github.com>
2025-04-18 15:36:14 +0700

    nambah login di navbar seng

--- resources/views/component/navbar.blade.php
+++ resources/views/component/navbar.blade.php
@@ -13,9 +13,25 @@
     <a href="/pendaftaran">
       <span class="orange-gradient rounded-full px-4 py-2 hover:bg-purple-950 text-purple-950 font-medium">Pendaftaran</span>
     </a>
-    <a href="/login">
-      <span class="orange-border px-4 py-2 text-login">Login</span>
-    </a>
+    @if (Route::has('login'))
+                <nav class="flex items-center justify-end gap-4">
+                    @auth
+                        <a
+                            href="{{ url('/dashboard') }}"
+                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                        >
+                            Dashboard
+                        </a>
+                    @else
+                        <a
+                            href="{{ route('login') }}"
+                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
+                        >
+                            Log in
+                        </a>
+                    @endauth
+                </nav>
+            @endif
     <span class="text-white text-xl">=</span>
   </div>
 </nav>
