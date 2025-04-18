c757022b4343b393ee71aaed8714633a96d7fe95
blayrublay <106981669+aqsayangkamu@users.noreply.github.com>
2025-04-18 15:24:57 +0700

    hadeh

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('homepage');
 });
-Route::get('/pendaftaran', function () {
-    return view('pendaftaran');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/pendaftaran/data-anak', function () {
-    return view('pendaftaran-dataAnak');
-});
-Route::get('/pendaftaran/data-ortu', function () {
-    return view('pendaftaran-dataOrtu');
-});
-Route::get('/pendaftaran/konfirmasi-pembayaran1', function () {
-    return view('pendaftaran-konfirmasiBayar');
-});
-Route::get('/pendaftaran/form-lanjutan', function () {
-    return view('pendaftaran-formLanjutan');
-});
+
+require __DIR__.'/auth.php';
