b0701f9375cacf094070abe1cb4eba52bd7ec126
blayrublay <106981669+aqsayangkamu@users.noreply.github.com>
2025-04-21 15:28:02 +0700

    db

---
+++ database/migrations/2025_04_18_111939_create_moms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('moms', function (Blueprint $table) {
+            $table->id();
+            $table->String('NIK');
+            $table->String('tempat lahir');
+            $table->date('tanggal_lahir');
+            $table->String('phone_number');
+            $table->String('email');
+            $table->String('jenjang_pendidikan');
+            $table->String('penghasilan');
+            $table->String('pekerjaan');
+            $table->timestamp('updated_at')->nullable('current_timestamp()');
+            $table->timestamp('created_at')->nullable('current_timestamp()');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('moms');
+    }
+};
