6c089114782bb1e6c35e7652ad7c497647b39018
nenematoda <natashadind@upi.edu>
2025-04-25 18:06:18 +0700

    page fasilitas done

---
+++ resources/views/fasilitas.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+@section('content')
+<div class="w-full h-72 relative bg-gray-700">
+    <img src="{{ asset('images/Banner.png') }}" class="absolute inset-0 w-full h-full opacity-80"/>
+    <h1 class="absolute inset-0 flex items-center justify-center text-white text-3xl font-bold">Fasilitas</h1>
+</div>
+<div class="relative my-20 flex flex-wrap justify-center gap-12">
+    <div class="relative rounded-[2rem]"  style="box-shadow: 0px 10px 5px 0px rgba(255,152,78,0.9);">
+        <img src="{{ asset('images/18.png') }}" alt="Fasilitas 1" class="relative z-0 w-96 h-72 rounded-[2rem] object-cover">
+        <p class="relative z-10 text-center text-xl font-semibold text-white rounded-br-[2rem] rounded-bl-[2rem] bg-gradient-to-t from-purple-900 to-purple-900/0 bg-opacity-50 -mt-[2.7rem] p-2">Fasilitas #1</p>
+    </div>
+    <div class="relative rounded-[2rem]"  style="box-shadow: 0px 10px 5px 0px rgba(255,152,78,0.9);">
+        <img src="{{ asset('images/18.png') }}" alt="Fasilitas 1" class="relative z-0 w-96 h-72 rounded-[2rem] object-cover">
+        <p class="relative z-10 text-center text-xl font-semibold text-white rounded-br-[2rem] rounded-bl-[2rem] bg-gradient-to-t from-purple-900 to-purple-900/0 bg-opacity-50 -mt-[2.7rem] p-2">Fasilitas #1</p>
+    </div>
+    <div class="relative rounded-[2rem]"  style="box-shadow: 0px 10px 5px 0px rgba(255,152,78,0.9);">
+        <img src="{{ asset('images/18.png') }}" alt="Fasilitas 1" class="relative z-0 w-96 h-72 rounded-[2rem] object-cover">
+        <p class="relative z-10 text-center text-xl font-semibold text-white rounded-br-[2rem] rounded-bl-[2rem] bg-gradient-to-t from-purple-900 to-purple-900/0 bg-opacity-50 -mt-[2.7rem] p-2">Fasilitas #1</p>
+    </div>
+    <div class="relative rounded-[2rem]"  style="box-shadow: 0px 10px 5px 0px rgba(255,152,78,0.9);">
+        <img src="{{ asset('images/18.png') }}" alt="Fasilitas 1" class="relative z-0 w-96 h-72 rounded-[2rem] object-cover">
+        <p class="relative z-10 text-center text-xl font-semibold text-white rounded-br-[2rem] rounded-bl-[2rem] bg-gradient-to-t from-purple-900 to-purple-900/0 bg-opacity-50 -mt-[2.7rem] p-2">Fasilitas #1</p>
+    </div>
+    <div class="relative rounded-[2rem]"  style="box-shadow: 0px 10px 5px 0px rgba(255,152,78,0.9);">
+        <img src="{{ asset('images/18.png') }}" alt="Fasilitas 1" class="relative z-0 w-96 h-72 rounded-[2rem] object-cover">
+        <p class="relative z-10 text-center text-xl font-semibold text-white rounded-br-[2rem] rounded-bl-[2rem] bg-gradient-to-t from-purple-900 to-purple-900/0 bg-opacity-50 -mt-[2.7rem] p-2">Fasilitas #1</p>
+    </div>
+    <div class="relative rounded-[2rem]"  style="box-shadow: 0px 10px 5px 0px rgba(255,152,78,0.9);">
+        <img src="{{ asset('images/18.png') }}" alt="Fasilitas 1" class="relative z-0 w-96 h-72 rounded-[2rem] object-cover">
+        <p class="relative z-10 text-center text-xl font-semibold text-white rounded-br-[2rem] rounded-bl-[2rem] bg-gradient-to-t from-purple-900 to-purple-900/0 bg-opacity-50 -mt-[2.7rem] p-2">Fasilitas #1</p>
+    </div>
+    <div class="relative rounded-[2rem]"  style="box-shadow: 0px 10px 5px 0px rgba(255,152,78,0.9);">
+        <img src="{{ asset('images/18.png') }}" alt="Fasilitas 1" class="relative z-0 w-96 h-72 rounded-[2rem] object-cover">
+        <p class="relative z-10 text-center text-xl font-semibold text-white rounded-br-[2rem] rounded-bl-[2rem] bg-gradient-to-t from-purple-900 to-purple-900/0 bg-opacity-50 -mt-[2.7rem] p-2">Fasilitas #1</p>
+    </div>
+    <div class="relative rounded-[2rem]"  style="box-shadow: 0px 10px 5px 0px rgba(255,152,78,0.9);">
+        <img src="{{ asset('images/18.png') }}" alt="Fasilitas 1" class="relative z-0 w-96 h-72 rounded-[2rem] object-cover">
+        <p class="relative z-10 text-center text-xl font-semibold text-white rounded-br-[2rem] rounded-bl-[2rem] bg-gradient-to-t from-purple-900 to-purple-900/0 bg-opacity-50 -mt-[2.7rem] p-2">Fasilitas #1</p>
+    </div>
+    <div class="relative rounded-[2rem]"  style="box-shadow: 0px 10px 5px 0px rgba(255,152,78,0.9);">
+        <img src="{{ asset('images/18.png') }}" alt="Fasilitas 1" class="relative z-0 w-96 h-72 rounded-[2rem] object-cover">
+        <p class="relative z-10 text-center text-xl font-semibold text-white rounded-br-[2rem] rounded-bl-[2rem] bg-gradient-to-t from-purple-900 to-purple-900/0 bg-opacity-50 -mt-[2.7rem] p-2">Fasilitas #1</p>
+    </div>
+    <div class="relative rounded-[2rem]"  style="box-shadow: 0px 10px 5px 0px rgba(255,152,78,0.9);">
+        <img src="{{ asset('images/18.png') }}" alt="Fasilitas 1" class="relative z-0 w-96 h-72 rounded-[2rem] object-cover">
+        <p class="relative z-10 text-center text-xl font-semibold text-white rounded-br-[2rem] rounded-bl-[2rem] bg-gradient-to-t from-purple-900 to-purple-900/0 bg-opacity-50 -mt-[2.7rem] p-2">Fasilitas #1</p>
+    </div>
+    <div class="relative rounded-[2rem]"  style="box-shadow: 0px 10px 5px 0px rgba(255,152,78,0.9);">
+        <img src="{{ asset('images/18.png') }}" alt="Fasilitas 1" class="relative z-0 w-96 h-72 rounded-[2rem] object-cover">
+        <p class="relative z-10 text-center text-xl font-semibold text-white rounded-br-[2rem] rounded-bl-[2rem] bg-gradient-to-t from-purple-900 to-purple-900/0 bg-opacity-50 -mt-[2.7rem] p-2">Fasilitas #1</p>
+    </div>
+</div>
+@endsection
