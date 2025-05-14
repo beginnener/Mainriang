06a4ed83febcc28d9ef60002e8b15fd91f16ed1b
blayrublay <106981669+aqsayangkamu@users.noreply.github.com>
2025-05-14 18:52:26 +0700

    jnambahin js pilihan

--- resources/views/pendaftaran-dataOrtu.blade.php
+++ resources/views/pendaftaran-dataOrtu.blade.php
@@ -6,6 +6,8 @@
     <!-- Form -->
     <div class="w-4/5 mx-auto">
         <form action="" class="flex flex-col gap-4">
+            @csrf
+            <input type="hidden" name="pendaftar" value="{{ $pendaftar }}">
             <!-- Nama Lengkap -->
             <div class="flex flex-row items-center">
                 <div class="w-1/6 flex justify-end items-center">
@@ -23,7 +25,7 @@
                 </div>
                 <span class="px-1 font-['Poppins'] flex items-center">:</span>
                 <div class="flex-grow flex items-center px-2">
-                    <input type="tel" id="no_telp_ibu" name="no_telp_ibu" pattern="^\+[0-9]{1,2}-[0-9]{10,11}" class="w-full h-16 px-6 bg-zinc-200 border-none rounded-full font-['Poppins'] text-left" placeholder="+62xxxxxxxxxx">
+                    <input type="tel" id="no_telp_ibu" name="no_telp_ibu" pattern="^\+[0-9]{1,2}-[0-9]{10,11}" class="w-full h-16 px-6 bg-zinc-200 border-none rounded-full font-['Poppins'] text-left" placeholder="+62">
                 </div>
             </div>
             <!-- Pekerjaan ibu -->
@@ -33,15 +35,15 @@
                 </div>
                 <span class="px-1 font-['Poppins'] flex items-center">:</span>
                 <div class="flex-grow flex items-center px-2">
-                    <select type="text" id="pekerjaan_ibu" name="pekerjaan_ibu" class="w-1/2 h-16 px-6 bg-zinc-200 border-none rounded-full font-['Poppins'] text-left" placeholder="Masukkan nama panggilan anak">
+                    <select type="text" id="pekerjaan_ibu" name="pekerjaan_ibu" class="w-full h-16 px-6 bg-zinc-200 border-none rounded-full font-['Poppins'] text-left transition-all duration-300 ease-in-out" placeholder="Masukkan nama panggilan anak">
                         <option value="Ibu Rumah Tangga">Ibu Rumah Tangga</option>
                         <option value="PNS">PNS</option>
                         <option value="Karyawan Swasta">Karyawan Swasta</option>
                         <option value="Wiraswasta">Wiraswasta</option>
                         <option value="Lainnya">Lainnya</option>
                     </select>
-                    <div class="pl-2 flex-grow flex">
-                        <input type="text" id="pekerjaan_ibu_lainnya" name="pekerjaan_ibu" class="w-full h-16 px-6 bg-zinc-200 border-none rounded-full font-['Poppins'] text-left" placeholder="Isi kolom ini jika memilih lainnya">
+                    <div class="hidden pl-2 flex-grow transition-all duration-300 ease-in-out" id="div_nyokap">
+                        <input type="text" id="pekerjaan_ibu_lainnya" name="pekerjaan_ibu" class="transition-all duration-300 ease-in-out max-h-0 opacity-0 overflow-hidden w-full h-16 px-6 bg-zinc-200 border-none rounded-full font-['Poppins'] text-left" placeholder="Isi kolom ini jika memilih lainnya">
                     </div>
                 </div>
             </div>
@@ -82,15 +84,15 @@
                 </div>
                 <span class="px-1 font-['Poppins'] flex items-center">:</span>
                 <div class="flex-grow flex items-center px-2">
-                    <select type="text" id="pekerjaan_ayah" name="pekerjaan_ayah" class="w-1/2 h-16 px-6 bg-zinc-200 border-none rounded-full font-['Poppins'] text-left" placeholder="Masukkan nama panggilan anak">
+                    <select type="text" id="pekerjaan_ayah" name="pekerjaan_ayah" class="w-full h-16 px-6 bg-zinc-200 border-none rounded-full font-['Poppins'] text-left transition-all duration-300 ease-in-out" placeholder="Masukkan nama panggilan anak">
                         <option value="Ayah Rumah Tangga">Ayah Rumah Tangga</option>
                         <option value="PNS">PNS</option>
                         <option value="Karyawan Swasta">Karyawan Swasta</option>
                         <option value="Wiraswasta">Wiraswasta</option>
                         <option value="Lainnya">Lainnya</option>
                     </select>
-                    <div class="pl-2 flex-grow flex">
-                        <input type="text" id="pekerjaan_ayah_lainnya" name="pekerjaan_ayah" class="w-full h-16 px-6 bg-zinc-200 border-none rounded-full font-['Poppins'] text-left" placeholder="Isi kolom ini jika memilih lainnya">
+                    <div class="hidden pl-2 flex-grow transition-all duration-300 ease-in-out" id="div_bokap">
+                        <input type="text" id="pekerjaan_ayah_lainnya" name="pekerjaan_ayah" class="max-h-0 opacity-0 overflow-hidden w-full h-16 px-6 bg-zinc-200 border-none rounded-full font-['Poppins'] text-left" placeholder="Isi kolom ini jika memilih lainnya">
                     </div>
                 </div>
             </div>
@@ -110,4 +112,38 @@
         </form>
     </div>
 </div>
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+    function handleSelectChange(selectId, inputId, divId) {
+        const select = document.getElementById(selectId);
+        const input = document.getElementById(inputId);
+        const div = document.getElementById(divId);
+
+        // Inisialisasi agar transisi width bisa berjalan
+        select.style.transition = 'width 0.3s ease';
+
+        select.addEventListener('change', function () {
+            const isLainnya = this.value === 'Lainnya';
+            if (isLainnya) {
+                select.style.width = '50%';
+                div.classList.remove('hidden');
+                div.classList.add('flex');
+                input.classList.remove('max-h-0', 'opacity-0', 'overflow-hidden');
+                input.classList.add('max-h-40', 'opacity-100');
+            } else {
+                select.style.width = '100%';
+                div.classList.add('hidden');
+                div.classList.remove('flex');
+                input.classList.add('max-h-0', 'opacity-0', 'overflow-hidden');
+                input.classList.remove('max-h-40', 'opacity-100');
+                input.value = '';
+            }
+        });
+    }
+
+    handleSelectChange('pekerjaan_ayah', 'pekerjaan_ayah_lainnya', 'div_bokap');
+    handleSelectChange('pekerjaan_ibu', 'pekerjaan_ibu_lainnya', 'div_nyokap');
+});
+</script>
+
 @endsection
