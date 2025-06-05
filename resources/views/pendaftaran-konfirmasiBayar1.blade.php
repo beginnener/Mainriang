5d6ded16692424b28dcf5d6eec56b82d5491a41e
nenematoda <natashadind@upi.edu>
2025-06-05 11:06:19 +0700

    ubah tombol dari kirim ulan pesan -> hubungi admin

--- resources/views/pendaftaran-konfirmasiBayar1.blade.php
+++ resources/views/pendaftaran-konfirmasiBayar1.blade.php
@@ -2,23 +2,24 @@
 <!-- Judul -->
 @section('title', 'Pembayaran Pendaftaran')
 @section('content')
-    <div class="w-full mb-10 flex flex-col pt-6 pb-10 justify-center overflow-hidden">
+    <div class="w-full my-8 flex flex-col pt-6 pb-10 justify-center overflow-hidden">
         <div class="mx-48 flex flex-row gap-6 items-center">
-            <div class="flex flex-col">
+            <div class="flex flex-col w-[1200px]">
                 <span class="font-['Poppins'] text-left text-xl">
-                    Admin akan menghubungi anda melalui whatsapp untuk detail pembayaran pendaftaran, mohon ditunggu....<br>
+                    Admin akan menghubungi anda melalui whatsapp untuk detail pembayaran pendaftaran, mohon ditunggu..<br>
+                </span>
+                <span class="font-['Poppins'] text-left text-lg text-amber-500">
                     (Halaman akan otomatis berubah setelah admin mengkonfirmasi pembayaran)
                 </span>
-                <span class="mt-6 font-['Poppins'] text-left text-xl">Jika tidak ada pesan yang masuk, silahkan klik tombol dibawah ini untuk mengirim ulang pesan.
+                <span class="mt-6 font-['Poppins'] text-left text-xl">Jika tidak ada pesan yang masuk, silahkan klik tombol dibawah ini untuk menghubungi admin.
                 </span>
                 <div class="my-2 flex items-center">
-                    {{-- tombol --}}
-                    <button type="button" class="inline-flex items-center justify-center w-grow h-8 px-8 rounded-full bg-white text-black font-bold text-center transition hover:bg-purple-900 border hover:text-white border-purple-950">
-                    Kirim ulang pesan
+                    <button type="button" class="inline-flex items-center justify-center w-grow py-4 px-8 rounded-full bg-white text-black font-bold text-center transition hover:bg-purple-900 border hover:text-white border-purple-950">
+                    Hubungi admin
                     </button>
                 </div>
             </div>
-            <span class="px-6 transform scale-125">
+            <span class="ml-10 px-6 transform scale-125">
                 <img src="{{ asset('icon/karakter 1.svg') }}" alt="" class="">
             </span>
         </div>
