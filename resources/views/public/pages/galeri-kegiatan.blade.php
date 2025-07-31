9fb870ff00df7ac63527aec952dfa327c442f280
nenematoda <natashadind@upi.edu>
2025-08-01 06:53:57 +0700

    update: relayout galeri kegiatan

--- resources/views/public/pages/galeri-kegiatan.blade.php
+++ resources/views/public/pages/galeri-kegiatan.blade.php
@@ -11,46 +11,12 @@
     ];
 @endphp
 
+<div class="w-full h-72 relative bg-gray-700">
+    <img src="{{ asset('images/Banner.png') }}" class="absolute inset-0 w-full h-full object-cover opacity-80"/>
+    <h1 class="absolute inset-0 flex items-center justify-center text-white text-3xl font-bold">Galeri Kegiatan</h1>
+</div>
+
 <div class="bg-purple-950"> 
-    <div class="relative">
-        <!-- Carousel Section -->
-        <div x-data="carousel({{ Js::from($images) }})"  class="relative w-full h-[70vh] mx-auto overflow-hidden shadow-lg touch-none focus:outline-none focus:border-transparent" role="region" aria-label="Image Carousel">
-            <!-- Carousel Images -->
-            <div class="flex transition-all duration-500 ease-in-out h-full" :style="`transform: translateX(-${active * 100}%)`">
-                <template x-for="(image, index) in images" :key="index">
-                    <div class="w-full flex-shrink-0 flex items-center justify-center">
-                        <img :src="image.src" class="w-full h-full object-cover object-center" loading="lazy" x-transition:enter="transition ease-in-out duration-500" x-transition:leave="transition ease-in-out duration-500" />
-                        <template x-if="image.heading || image.description">
-                            <div :class="`absolute ${image.textpos}`" class="w-fit h-fit bg-black/20 border border-white backdrop-blur-sm rounded-[2rem]">
-                                <div class="p-8">
-                                    <span class="font-['Poppins'] text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-l from-orange-400 to-amber-200" x-text="image.heading" x-show="image.heading"></span>
-                                    <div class="mt-2 w-[30rem]">
-                                        <span class="font-['Poppins'] text-base text-white" x-text="image.description" x-show="image.description"></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </template>
-                    </div>
-                </template>
-            </div>
-            
-            <!-- Prev / Next Buttons -->
-            <button @click="prev()" class="absolute z-20 top-1/2 left-4 -translate-y-1/2 bg-black/40 text-white p-4 text-xl rounded-full hover:bg-black/60 transition-all">
-                &#10094;
-            </button>
-            <button @click="next()" class="absolute z-20 top-1/2 right-4 -translate-y-1/2 bg-black/40 text-white p-4 text-xl rounded-full hover:bg-black/60 transition-all">
-                &#10095;
-            </button>
-            
-            <!-- Dots Navigation -->
-            <div class="absolute z-20 bottom-4 w-full flex justify-center space-x-2">
-                <template x-for="(image, index) in images" :key="index">
-                    <div @click="goTo(index)" :class="{ 'w-8 rounded-md bg-white': index === active, 'bg-gray-400': index !== active }" class="w-3 h-3 rounded-full cursor-pointer transition-all duration-700"></div>
-                </template>
-            </div>
-        </div>
-    </div>
-    
     <!-- Card Section -->
     <div class="w-full h-[400px] inset-0 bg-purple-950 flex items-center justify-center">
         <div class="py-10 flex flex-row gap-10">
@@ -74,67 +40,33 @@
         $images = [
             ['src' => asset('images/anak sholat 2.jpg'), 
                 'heading' => 'Kegiatan Eventual 1', 
-                'textpos' => '-ml-[45rem] mt-60',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.' ],
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.' ,
+                'textpos' => 'bottom-16 left-16'
+            ],
             ['src' => asset('images/belajar montessori 4.jpg'), 
                 'heading' => 'Kegiatan Eventual 1', 
-                'textpos' => 'ml-[43rem] mt-48',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.' ],
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.' ,
+                'textpos' => 'bottom-16 left-16'
+            ],
             ['src' => asset('images/WhatsApp Image 2025-03-17 at 14.09.44_b88173d4.jpg'), 
                 'heading' => 'Kegiatan Eventual 1', 
-                'textpos' => 'ml-[43rem] mt-52',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.' ],
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.' ,
+                'textpos' => 'bottom-16 left-16'
+            ],
             ['src' => asset('images/foto famgath.jpg'), 
                 'heading' => 'Kegiatan Eventual 1', 
-                'textpos' => '-ml-[45rem] -mt-60',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.' ],
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.' ,
+                'textpos' => 'top-16 left-16'
+            ],
+            ['src' => asset('images/foto famgath.jpg'), 
+                'heading' => 'Kegiatan Eventual 1', 
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.' ,
+                'textpos' => 'top-16 left-16'
+            ],
         ];
     @endphp
-    
-    <div class="relative">
-        <!-- top Gradient Overlay -->
-        <div class="absolute inset-x-0 top-0 h-[300px] bg-gradient-to-b from-purple-950 via-transparent to-transparent z-10"></div>
-        
-        <!-- Carousel Section -->
-        <div x-data="carousel({{ Js::from($images) }})" class="relative w-full h-[44rem] mx-auto overflow-hidden shadow-lg touch-none focus:outline-none focus:border-transparent" role="region" aria-label="Image Carousel">
-            <!-- Carousel Images -->
-            <div class="flex transition-all duration-500 ease-in-out h-full" :style="`transform: translateX(-${active * 100}%)`">
-                <template x-for="(image, index) in images" :key="index">
-                    <div class="w-full flex-shrink-0 flex items-center justify-center">
-                        <img :src="image.src" class="w-full h-full object-cover object-center" loading="lazy" x-transition:enter="transition ease-in-out duration-500" x-transition:leave="transition ease-in-out duration-500" />
-                        <template x-if="image.heading || image.description">
-                            <div :class="`absolute ${image.textpos}`" class="w-fit h-fit bg-black/20 border border-white backdrop-blur-sm rounded-[2rem]">
-                                <div class="p-8">
-                                    <span class="font-['Poppins'] text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-l from-orange-400 to-amber-200" x-text="image.heading" x-show="image.heading"></span>
-                                    <div class="mt-2 w-[30rem]">
-                                        <span class="font-['Poppins'] text-base text-white" x-text="image.description" x-show="image.description"></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </template>
-                    </div>
-                </template>
-            </div>
-            
-            <!-- Prev / Next Buttons -->
-            <button @click="prev()" class="absolute z-20 top-1/2 left-4 -translate-y-1/2 bg-black/40 text-white p-4 text-xl rounded-full hover:bg-black/60 transition-all">
-                &#10094;
-            </button>
-            <button @click="next()" class="absolute z-20 top-1/2 right-4 -translate-y-1/2 bg-black/40 text-white p-4 text-xl rounded-full hover:bg-black/60 transition-all">
-                &#10095;
-            </button>
-            
-            <!-- Dots Navigation -->
-            <div class="absolute z-20 bottom-4 w-full flex justify-center space-x-2">
-                <template x-for="(image, index) in images" :key="index">
-                    <div @click="goTo(index)" :class="{ 'w-8 rounded-md bg-white': index === active, 'bg-gray-400': index !== active }" class="w-3 h-3 rounded-full cursor-pointer transition-all duration-700"></div>
-                </template>
-            </div>
-        </div>
-        
-        <!-- Bottom Gradient Overlay -->
-        <div class="absolute inset-x-0 bottom-0 h-[300px] bg-gradient-to-t from-purple-950 via-transparent to-transparent z-10"></div>
-    </div>
+
+    <x-carousel :images="$images" />
 
     @php
     $videos = [
@@ -144,7 +76,7 @@
     ];
     @endphp
 
-    <div class="relative z-10 bg-white rounded-t-[3rem]">
+    <div class="relative z-10 bg-white">
         <div class="py-10 flex flex-row">
             <div class="mb-8 flex justify-center items-center w-[800px]">
                 <span class="font-['Poppins'] text-3xl font-bold text-purple-950 block text-right">Galeri Video</span>
