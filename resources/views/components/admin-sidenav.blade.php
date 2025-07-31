29c14716c69b557b66ad02a8c64b7b2a3293e8a1
nenematoda <natashadind@upi.edu>
2025-08-01 01:41:43 +0700

    update: only take the first word of user name

--- resources/views/components/admin-sidenav.blade.php
+++ resources/views/components/admin-sidenav.blade.php
@@ -3,7 +3,7 @@
   <div>
     <div class="flex flex-row gap-2 items-center w-full py-3 px-8 mb-4">
         <img src="{{ asset('icon/logo main riang.svg') }}" alt="" class="w-12 h-12">
-        <span class="text-xl font-bold font-['Fredoka'] text-white ml-2 text-wrap">Halo, {{ Str::title(Auth::user()->name) }}</span>
+        <span class="text-xl font-bold font-['Fredoka'] text-white ml-2 text-wrap">Halo, {{ Str::title(explode(' ', Auth::user()->name)[0]) }}</span>
     </div>
     <a href="{{ route('admin-dashboard') }}"
       class="sidebar-button flex items-center w-full py-3 px-8 leading-tight font-['Poppins'] transition-all outline-none text-start hover:bg-purple-950 hover:text-amber-400 focus:bg-purple-950 focus:text-amber-400 active:bg-purple-950 active:text-amber-400
