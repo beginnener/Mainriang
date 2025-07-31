3d4b7069aac832a63eae712a27365d9b4024feb6
nenematoda <natashadind@upi.edu>
2025-08-01 06:43:48 +0700

    update: penyesuaian navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -33,7 +33,7 @@
       </div>
     </div>
     <a href="{{ route('fasilitas') }}">
-      <span id="toggleFasilitas" class="px-2 text-white text-base font-medium font-['Poppins'] transition hover:text-yellow-400 active:text-orange-500 focus:text-yellow-400">
+      <span class="px-2 text-white text-base font-medium font-['Poppins'] transition hover:text-yellow-400 active:text-orange-500 focus:text-yellow-400">
         Fasilitas
       </span>
     </a>
@@ -53,35 +53,12 @@
       </div>
     </a>
     @if (Route::has('login'))
-      @auth
-      <div class="relative">
-        <div id="toggleAllMenu" class="flex p-4 rounded-lg items-center border-transparent hover:border-2 hover:border-purple-800 hover:bg-purple-950 active:border-2 active:border-purple-800 active:bg-purple-950 focus:border-2 focus:border-purple-800 focus:bg-purple-950">
-          <img src="{{ asset('icon/All.svg') }}">
-        </div>
-        {{-- dropdown menu untuk profil --}}
-        <div id= "allMenu" class="hidden absolute top-full right-1 z-10 mt-2 w-[180px] bg-white rounded-3xl shadow-lg ring-1 ring-gray-900/5">
-          <div class="p-4">
-            <div class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-slate-100">
-              <div class="flex-auto">
-                <a href="{{ route('dashboard') }}" class="block font-semibold text-gray-900 group-hover:text-orange-400">
-                  Dashboard
-                  <span class="absolute inset-0"></span>
-                </a>
-              </div>
-            </div>
-            <div class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-slate-100">
-              <div class="flex-auto">
-                <form method="POST" action="{{ route('logout') }}">
-                  @csrf
-                  <button type="submit" class="block font-semibold text-gray-900 hover:text-orange-400">
-                      Logout
-                  </button>
-              </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    @auth
+    <a href="{{ route('dashboard') }}">
+      <span class="px-2 text-white text-base font-medium font-['Poppins'] transition hover:text-yellow-400 active:text-orange-500 focus:text-yellow-400">
+        Dashboard
+      </span>
+    </a>
     @else
       <a href="{{ route('login') }}">
         <div class="w-28 h-10 flex items-center justify-center rounded-full bg-purple-900 text-orange-400 font-medium font-['Poppins'] border border-orange-400 transition duration-500 hover:text-purple-900 hover:border-none hover:bg-gradient-to-l hover:from-orange-400 hover:to-amber-300 group active:bg-amber-400 active:text-white">
